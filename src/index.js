30b28ae9326bf9649b9b2ac3d6fc4dbbed58d599
June Domingo <june@cynder.io>
2016-07-26 02:29:25 +0800

    Deleted lib folder from assets; referenced bootstrap and ionicons to bower_components

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'toastr/build/toastr.min.css';
-import './assets/lib/ionicons/css/ionicons.min.css';
+import '../bower_components/bootstrap/dist/css/bootstrap.min.css';
+import '../bower_components/Ionicons/css/ionicons.min.css';
 import './assets/styl/main.styl';
  
 const store = configureStore();
