8de889c69216afb713d6d617bf39d6dee67172f9
June Domingo <june@cynder.io>
2016-07-27 12:17:01 +0800

    Changed 'checkDotEnv' implementation

--- utils/checkDotEnv.js
+++ utils/checkDotEnv.js
@@ -5,10 +5,10 @@ import colors from 'colors';
 
 fs.stat('.env', function(err, stat) {
 	if(err == null) {
-		console.log('Starting app in dev mode...'.green);
+		// .env is set
 	} else if (err.code == 'ENOENT') {
 		fs.createReadStream('.env.sample').pipe(fs.createWriteStream('.env'));
-		console.log('Generated .env file in root directory.'.yellow + '\n' + 'Starting app in dev mode...It will automatically open your default browser.'.green);
+		console.log('Generated .env file in root directory.'.yellow);
 	} else {
 		console.log('Something went wrong: ', err.code);
 		process.exit(1);
