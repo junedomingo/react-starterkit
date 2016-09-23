89128b19bb11b5361832e08426db3ffb19f1fb22
June Domingo <june@cynder.io>
2016-09-23 19:47:39 +0800

    Initial commit

---
+++ utils/postMessage.js
@@ -0,0 +1,5 @@
+/*eslint-disable no-console*/
+import colors from 'colors';
+
+console.log('Your app has been compiled in production mode and written to /dist.'.green);
+console.log('Type "npm run prod" to start production server.'.yellow);
