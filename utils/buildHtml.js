b500a24b2f4f58ab5f2b5152e74254064269c0d0
June Domingo <june@cynder.io>
2016-11-20 12:58:13 +0800

    Added ‘/’ to fix ‘Resource interpreted as Stylesheet but transferred with MIME type text/html’ issue

--- utils/buildHtml.js
+++ utils/buildHtml.js
@@ -11,13 +11,13 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
 
 	const $ = cheerio.load(markup);
 
-	$('head').prepend(`<link rel="stylesheet" href="assets/css/styles.css">`);
+	$('head').prepend(`<link rel="stylesheet" href="/assets/css/styles.css">`);
 
 	$('script').each(function () {
 		$(this).remove();
 	});
 
-	$('body').append(`<script src="assets/js/bundle.js"></script>`);
+	$('body').append(`<script src="/assets/js/bundle.js"></script>`);
 
 	fs.writeFile('dist2/index.html', $.html(), 'utf8', err => {
 		if (err) return console.log(err);
